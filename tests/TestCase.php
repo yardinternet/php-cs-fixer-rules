3620a22262e9625dd65c1c63021060b6dfe4393b
Freek de Kruijff <freek.dekruijff@yard.nl>
2024-11-14 12:59:17 +0100

    (chore): Pest and TestCase.php

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,35 +4,9 @@ declare(strict_types=1);
 
 namespace Yard\PhpCsFixerRules\Tests;
 
-use Orchestra\Testbench\TestCase as Orchestra;
+use PHPUnit\Framework\TestCase as BaseTestCase;
 
-class TestCase extends Orchestra
+abstract class TestCase extends BaseTestCase
 {
-    protected function setUp(): void
-    {
-        parent::setUp();
-
-        \WP_Mock::setUp();
-    }
-
-    protected function tearDown(): void
-    {
-        parent::tearDown();
-
-        \WP_Mock::tearDown();
-    }
-
-    /**
-     * Get package providers.
-     *
-     * @param  \Illuminate\Foundation\Application  $app
-     *
-     * @return array<int, class-string<\Illuminate\Support\ServiceProvider>>
-     */
-    protected function getPackageProviders($app)
-    {
-        return [
-            'Yard\PhpCsFixerRules\PhpCsFixerRulesServiceProvider',
-        ];
-    }
+    //
 }
